d7fba2dedbbc03a7f9f8bce8834c418cce51c6e3
Eykr11 <160566887+Eykr11@users.noreply.github.com>
2025-04-19 16:03:48 -0500

    Actualización de frontend y backend: nuevos archivos, cambios en rutas y estilos

--- backend/src/controllers/categorias.controllers.js
+++ backend/src/controllers/categorias.controllers.js
@@ -11,7 +11,73 @@ try {
 }
 }
 
+const postCategorrias = async (req,res) =>{
+    try {
+        const {CategoriaNombre, Descripcion, Imagen} = (req.body);
+        const category ={
+            CategoriaNombre,
+            Descripcion,
+            Imagen  
+        }
+        const connection = await getConnection();
+        const result = await connection.query("INSERT INTO categorias SET ?", category)
 
+
+
+        res.json(result)
+    } catch (error) {
+        console.error("ERROR 500");
+    }
+}
+const  getCategory = async (req, res)=>{
+    try {
+        console.log(req.params);
+        const {id} = req.params
+        const connection = await getConnection();
+        const result = await connection.query("SELECT CategoriaID,CategoriaNombre ,Descripcion,Imagen FROM categorias WHERE CategoriaID = ?", id )
+        res.json(result);
+    }    
+     catch (error) {
+        console.error("ERROR 500")
+    }
+    }
+    const  deleteCategory = async (req, res)=>{
+        try {
+            console.log("id de la categoria a borrar ",req.params);
+            const {id} = req.params
+            const connection = await getConnection();
+            const result = await connection.query("DELETE CategoriaID,CategoriaNombre ,Descripcion,Imagen FROM categorias WHERE CategoriaID = ?", id )
+            res.json(result);
+        }    
+         catch (error) {
+            console.error("ERROR 500")
+        }
+        }
+
+        const updatetCategorrias = async (req,res) =>{
+            try {
+                const {id} = req.params
+                const {CategoriaNombre, Descripcion, Imagen} = (req.body);
+                const category ={
+                    CategoriaNombre,
+                    Descripcion,
+                    Imagen  
+                }
+                const connection = await getConnection();
+                const result = await connection.query("UPDATE categorias SET ? WHERE CategoriaID = ?",[category,id] )
+        
+        
+        
+                res.json(result)
+            } catch (error) {
+                console.error("ERROR 500");
+            }
+        }
 export const methodHTTP = {
-    getCategorias
+    getCategorias,
+    postCategorrias,
+    getCategory,
+    deleteCategory,
+    updatetCategorrias
+    
 }
--- backend/src/routes/categorias.routes.js
+++ backend/src/routes/categorias.routes.js
@@ -2,6 +2,11 @@ import {Router} from "express"
 import { methodHTTP as categoriaController } from "../controllers/categorias.controllers.js";
 const router = Router();
 
-router.get("/", categoriaController.getCategorias)
-
+router.get("/", categoriaController.getCategorias);
+router.post("/", categoriaController.postCategorrias);
+//ruta de un solo parametro get
+router.get("/:id", categoriaController.getCategory);
+router.delete("/:id", categoriaController.deleteCategory);
+//ruta con parametro tanto el id como el cuerpo para actualizar
+router.put("/:id", categoriaController.updatetCategorrias);
 export default router;
